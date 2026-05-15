4b11f54fc0658a982ee1b6978ff0866846933f1a
davidChibueze <davidtony141@gmail.com>
2026-05-15 02:32:59 +0100

    migration v1

--- artist-admin/src/migrations/index.ts
+++ artist-admin/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260513_082836_initial from './20260513_082836_initial';
 import * as migration_20260515_012004_add_s3_prefix_to_media from './20260515_012004_add_s3_prefix_to_media';
+import * as migration_20260515_013117 from './20260515_013117';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260515_012004_add_s3_prefix_to_media.up,
     down: migration_20260515_012004_add_s3_prefix_to_media.down,
-    name: '20260515_012004_add_s3_prefix_to_media'
+    name: '20260515_012004_add_s3_prefix_to_media',
+  },
+  {
+    up: migration_20260515_013117.up,
+    down: migration_20260515_013117.down,
+    name: '20260515_013117'
   },
 ];
